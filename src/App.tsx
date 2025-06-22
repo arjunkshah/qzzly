6e60ecd2e692c26d4402a5a1890d852f0b50b90d
Arjun Shah <arjunkshah21@Arjuns-MacBook-Air.local>
2025-06-22 09:57:23 -0700

    fix: add future flags to react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,7 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route element={<ProtectedRoute redirectTo="/" />}> 
