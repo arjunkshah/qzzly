0de0bab1250b78ce7ebdb46b1daec2a96e0fa272
Arjun Shah <arjunkshah21@Arjuns-MacBook-Air.local>
2025-07-06 20:29:04 -0700

    fix: set CORS to allow https://qzzly.com and localhost:3000

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ const upload = multer();
 
 // Middleware
 app.use(cors({
-  origin: ["https://qzzly.com", "http://localhost:3000"],
+  origin: ['https://qzzly.com', 'http://localhost:3000'],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "Origin", "Accept"],
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'dist')));
